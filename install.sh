0a55425ceb02de1d8f6d796ffab71e360dd18e25
AlexString <ricardo.lopez2962@alumnos.udg.mx>
2021-11-20 18:28:51 -0600

    script grep_aliases.py created

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install configuration files
+
+# On root
+cp ./bash_aliases ~/
+cp ./scripts/ ~/
+# On .config
+cp ./alacritty.yml ~/.config/alacritty/
+#cp ./rofi ~/.config/ # rofi version should be updated
