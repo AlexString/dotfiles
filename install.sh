461cc7900b4d562ed1d68cec98199b22ad0b6435
AlexString <ricardo.lopez2962@alumnos.udg.mx>
2021-11-23 19:10:38 -0600

    Added nvim installation

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ cp ./scripts/ ~/
 # On .config
 cp ./alacritty.yml ~/.config/alacritty/
 #cp ./rofi ~/.config/ # rofi version should be updated
+# install nvim configuration from my repo
+git clone https://github.com/AlexString/MyNeoVim ~/.config/nvim/
