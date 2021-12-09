44c65979bd76dd7491c4d74a32ad7e6eccc9677d
AlexString <github.com/AlexString>
2021-12-09 15:19:57 -0600

    refactor and export path indication

--- install.sh
+++ install.sh
@@ -4,9 +4,15 @@
 
 # On root
 cp ./bash_aliases ~/
+echo "copied bash aliases"
+
+# To make your scripts callable copy this line to you .bashrc
+# export PATH="/home/username/scripts:$PATH"
 cp ./scripts/ ~/
+echo "copied scripts"
+
 # On .config
-cp ./alacritty.yml ~/.config/alacritty/
+#cp ./alacritty.yml ~/.config/alacritty/
 #cp ./rofi ~/.config/ # rofi version should be updated
 # install nvim configuration from my repo
-git clone https://github.com/AlexString/MyNeoVim ~/.config/nvim/
+#git clone https://github.com/AlexString/MyNeoVim ~/.config/nvim/
