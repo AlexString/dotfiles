2c70d98c4a0acf7971cce69f48838d29174b44af
alexstring <github.com/Alexstring>
2021-12-13 16:53:49 -0600

    moved scripts to scripts repository

--- install.sh
+++ install.sh
@@ -2,17 +2,21 @@
 
 # install configuration files
 
-# On root
-cp ./bash_aliases ~/
-echo "copied bash aliases"
+echo "copying bash aliases..."
+cp ./.bash_aliases ~/
 
 # To make your scripts callable copy this line to you .bashrc
 # export PATH="/home/username/scripts:$PATH"
-cp ./scripts/ ~/
-echo "copied scripts"
+
+echo "copying scripts..."
+cp -r ./scripts ~/
 
 # On .config
-#cp ./alacritty.yml ~/.config/alacritty/
-#cp ./rofi ~/.config/ # rofi version should be updated
-# install nvim configuration from my repo
-#git clone https://github.com/AlexString/MyNeoVim ~/.config/nvim/
+
+echo "copying rofi config..."
+cp -r ./rofi ~/.config/
+
+#echo "installing nvim configuration..." 
+#git clone https://github.com/AlexString/MyNeoVim ~/.config/nvim/.
+
+echo "script done."
--- scripts/aliascheatsheet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 ./python/grep_aliases.py
